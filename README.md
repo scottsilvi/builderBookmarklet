c58f4011a703da039f7e5bd60dfcd2772e99679e
scottsilvi <scottsilvi@gmail.com>
2014-12-31 15:47:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# LeadPages&trade; Builder Bookmarklet
+
+This Bookmarklet exposes keybinding functionality for manipulating the builder. It utilizes [Mousetrap](http://craig.is/killing/mice) for the bindings. 
+
+## Installation
+
+Just add the bookmarklet below to your bookmarks bar: 
+```javascript
+javascript:(function(){var el=document.createElement('script');el.src='https://zeman.github.io/perfmap/perfmap.js';document.body.appendChild(el);})();
+```
+
+## Usage
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+TODO: Write license
---
+++ builderKeybinding.js
@@ -0,0 +1,96 @@
+javascript: void
+function() {
+	alert('sup bitches!');
+	var jsCode = document.createElement('script'), styleArr = [],
+	head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+	style.type = 'text/css';
+	jsCode.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap.js');
+
+	styleArr.push('.currentInteractiveElement { border: 3px solid red !important; }');           
+
+	if (style.styleSheet){
+	  style.styleSheet.cssText = styleArr.join('');
+	} else {
+	  style.appendChild(document.createTextNode(styleArr.join('')));
+	}
+
+	head.appendChild(style);
+	document.body.appendChild(jsCode);
+
+	var currentElement;
+
+	setTimeout(function () {
+
+		var toggleInteractiveClass = function (toggle) {
+			currentElement.toggleClass('currentInteractiveElement', toggle);
+		};
+
+		Mousetrap.bind('ctrl+shift+i', function (e) {
+			Mousetrap.reset();
+			// collapse all
+			$('.expand.fa-minus-square').trigger('click');
+
+			currentElement = $('.list-group-item').eq(0);
+			toggleInteractiveClass(true);
+			
+			Mousetrap.bind('up', function (e) {
+				toggleInteractiveClass(false);
+				currentElement = currentElement.prev();
+				toggleInteractiveClass(true);
+			});
+			
+			Mousetrap.bind('down', function (e) {
+				toggleInteractiveClass(false);
+				currentElement = currentElement.next();
+				toggleInteractiveClass(true);
+			});
+			
+			Mousetrap.bind('left', function (e) {
+				toggleInteractiveClass(false);
+				currentElement.find('.icon .expand.fa-minus-square').trigger('click');
+				toggleInteractiveClass(true);
+			});
+			
+			Mousetrap.bind('space', function (e) {
+			 	eye = currentElement.find('.controls .glyphicon-eye-open');
+			 	if (eye.length) {
+					currentElement.find('.controls .glyphicon-eye-open').trigger('click');
+			 	} else {
+					currentElement.find('.controls .glyphicon-eye-close').trigger('click');
+			 	}
+			});
+			
+			Mousetrap.bind('right', function (e) {
+				toggleInteractiveClass(false);
+				currentElement.find('.icon .expand.fa-plus-square').trigger('click');
+				toggleInteractiveClass(true);
+			});
+
+			return false;
+		});
+		// Mousetrap.bind('ctrl+shift+h', function (e) {
+		// 	$('#editable-elements .list-group-item:not(:has(.indent)) .controls .glyphicon-eye-open').each(function (idx) {
+		// 		console.log('idx: ', 500 * idx);
+		// 		setTimeout(function () {
+		// 			$(this).trigger('click');
+		// 		}, 2000 * idx);
+		// 	});
+		// 	return false;
+		// });
+		// Mousetrap.bind('ctrl+shift+s', function (e) {
+		// 	$('#editable-elements .list-group-item:not(:has(.indent)) .controls .glyphicon-eye-close').each(function (idx) {
+
+		// 		$(this).trigger('click');
+		// 	});
+		// 	return false;
+		// });
+		// Mousetrap.bind('ctrl+shift+e', function (e) {
+		// 	$('.list-group-item .icon .expand.fa-plus-square').trigger('click');
+		// 	console.log('clicking plus');
+		// 	return false;
+		// });
+	}, 2000);
+
+}();
